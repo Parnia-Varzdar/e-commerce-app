2678d9e931c5d23b669ae0221e06a005b3de1ea3
parnia <parnia@parnia>
2024-08-06 22:54:26 +0330

    feat(project): implement config server

---
+++ services/config-server/src/test/java/com/parnia/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parnia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

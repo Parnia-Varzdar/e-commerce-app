65e9313cd2cb9010bbcb409df2b7b616777d6272
parnia <parnia@parnia>
2024-08-07 08:41:51 +0330

    feat(customer-service): implement customer service

---
+++ services/customer/src/main/java/com/parnia/ecommerce/customer/Address.java
@@ -0,0 +1,13 @@
+package com.parnia.ecommerce.customer;
+
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+@Builder
+@Setter
+@Getter
+public class Address {
+}
---
+++ services/customer/src/main/java/com/parnia/ecommerce/customer/CustomerService.java
@@ -0,0 +1,65 @@
+package com.parnia.ecommerce.customer;
+
+import com.parnia.ecommerce.exception.CustomerNotFoundException;
+import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository repository;
+    private final CustomerMapper mapper;
+
+    public String createCustomer(CustomerRequest request) {
+        var customer = repository.save(mapper.toCustomer(request));
+        return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = repository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        String.format("Cannot update customer:: No customer found with the provided ID: %s", request.id())
+                ));
+        mergeCustomer(customer, request);
+        repository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstname())) {
+            customer.setFirstname(request.firstname());
+        }
+        if (StringUtils.isNotBlank(request.email())) {
+            customer.setEmail(request.email());
+        }
+        if (request.address() != null) {
+            customer.setAddress(request.address());
+        }
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public CustomerResponse findById(String id) {
+        return repository.findById(id)
+                .map(mapper::fromCustomer)
+                .orElseThrow(() -> new CustomerNotFoundException(String.format("No customer found with the provided ID: %s", id)));
+    }
+
+    public boolean existsById(String id) {
+        return repository.findById(id)
+                .isPresent();
+    }
+
+    public void deleteCustomer(String id) {
+        repository.deleteById(id);
+    }
+}
